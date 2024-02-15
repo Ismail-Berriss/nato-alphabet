dc7ad84b5ad700ff9b580555792b5a6c61c9d21c
ismail berriss <ismailberriss@gmail.com>
2024-02-15 15:24:00 +0100

    Handling the "KeyError" exception.

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabets = {row.letter: row.code for (index, row) in data.iterrows()}
 
-user_input = input("Enter a word: ").upper()
-phonetic_code = [alphabets[letter] for letter in user_input]
-print(phonetic_code)
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        phonetic_code = [alphabets[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        is_input_valid = True
+        print(phonetic_code)
+
+
+generate_phonetic()
